56a46077bca6661310cdb11d8a444fa5dd1cd7d7
smartman1234 <john.smith.dev0724@gmail.com>
2023-01-31 20:36:44 +0800

    added validation util

---
+++ server/app/Utils/ValidationUtil.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Utils;
+
+class ValidationUtil
+{
+    public static function validateEmail(string $value = null)
+    {
+        if (empty($value)) {
+            return 'Please enter your email';
+        }
+        if (strlen($value) > 255) {
+            return 'Max 255 characters only';
+        }
+        if (!filter_var($value, FILTER_VALIDATE_EMAIL)) {
+            return 'Invalid email';
+        }
+        return null;
+    }
+
+    public static function validatePassword(string $value = null)
+    {
+        if (empty($value)) {
+            return 'Please enter a password';
+        }
+        if (strlen($value) < 8) {
+            return 'Password must be 8 characters and above';
+        }
+        if (strlen($value) > 255) {
+            return 'Max 255 characters only';
+        }
+        return null;
+    }
+}
