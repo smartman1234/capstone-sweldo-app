854a0e6a581a9480c455ab53e6cf4ae4b5e4a953
smartman1234 <john.smith.dev0724@gmail.com>
2023-01-31 21:08:52 +0800

    added department routes and controller

---
+++ server/app/Http/Controllers/Admin/DepartmentController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class DepartmentController extends Controller
+{
+    public function index(Request $request)
+    {
+        // TODO: Return departments
+    }
+
+    public function store(Request $request)
+    {
+        // TODO: Save department
+    }
+
+    public function show(Request $request)
+    {
+        // TODO: Return department by id
+    }
+
+    public function update(Request $request)
+    {
+        // TODO: Update department by id
+    }
+}
--- server/routes/api.php
+++ server/routes/api.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\LoginController;
 
 use App\Http\Controllers\Admin\AdminDashboardController;
+use App\Http\Controllers\Admin\DepartmentController;
 use App\Http\Controllers\Admin\EmployeeController;
 
 Route::post('/login', [LoginController::class, 'login']);
@@ -33,4 +34,10 @@ Route::group([
     Route::post('/employee', [EmployeeController::class, 'store']);
     Route::get('/employee/{id}', [EmployeeController::class, 'show']);
     Route::post('/employee/{id}', [EmployeeController::class, 'update']);
+
+    // Department
+    Route::get('/department', [DepartmentController::class, 'index']);
+    Route::post('/department', [DepartmentController::class, 'store']);
+    Route::get('/department/{id}', [DepartmentController::class, 'show']);
+    Route::post('/department/{id}', [DepartmentController::class, 'update']);
 });
