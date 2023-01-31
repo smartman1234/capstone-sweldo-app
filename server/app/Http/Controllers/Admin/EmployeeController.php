aea820b79ad46d0c6d9aed2e3046cb1563410df7
smartman1234 <john.smith.dev0724@gmail.com>
2023-01-31 21:06:50 +0800

    added employee routes and controller

---
+++ server/app/Http/Controllers/Admin/EmployeeController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class EmployeeController extends Controller
+{
+    public function index(Request $request)
+    {
+        // TODO: Return employees
+    }
+
+    public function store(Request $request)
+    {
+        // TODO: Save employee
+    }
+
+    public function show(Request $request)
+    {
+        // TODO: Return employee by id
+    }
+
+    public function update(Request $request)
+    {
+        // TODO: Update employee by id
+    }
+}
--- server/routes/api.php
+++ server/routes/api.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\LoginController;
 
 use App\Http\Controllers\Admin\AdminDashboardController;
+use App\Http\Controllers\Admin\EmployeeController;
 
 Route::post('/login', [LoginController::class, 'login']);
 
@@ -27,5 +28,9 @@ Route::group([
     // Dashboard
     Route::get('/dashboard', [AdminDashboardController::class, 'index']);
 
-
+    // Employee
+    Route::get('/employee', [EmployeeController::class, 'index']);
+    Route::post('/employee', [EmployeeController::class, 'store']);
+    Route::get('/employee/{id}', [EmployeeController::class, 'show']);
+    Route::post('/employee/{id}', [EmployeeController::class, 'update']);
 });
