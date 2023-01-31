3ebc69e1c8eb66a246eec61ba3f7fcf5ce7c4a3e
smartman1234 <john.smith.dev0724@gmail.com>
2023-01-31 21:16:11 +0800

    added custom input component

---
+++ client/src/components/ui/inputs/CustomInput.jsx
@@ -0,0 +1,37 @@
+const CustomInput = ({
+  label,
+  id,
+  type,
+  placeholder,
+  value,
+  onChange,
+  error,
+}) => {
+  return (
+    <div>
+      {label !== undefined && (
+        <label
+          htmlFor={id}
+          className='block text-gray-700 text-sm font-medium mb-2'
+        >
+          {label}
+        </label>
+      )}
+      <input
+        className={
+          error
+            ? 'w-full text-gray px-5 py-2.5 rounded border border-red-500'
+            : 'w-full text-gray px-5 py-2.5 rounded border'
+        }
+        id={id}
+        type={type}
+        placeholder={placeholder}
+        value={value}
+        onChange={onChange}
+      />
+      {error && <span className='text-sm text-red-500'>{error}</span>}
+    </div>
+  )
+}
+
+export default CustomInput
