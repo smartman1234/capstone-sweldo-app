3578ea72978506111f81a2e75073d5bf48764061
smartman1234 <john.smith.dev0724@gmail.com>
2023-01-31 20:35:39 +0800

    changed back to email

--- server/app/Models/User.php
+++ server/app/Models/User.php
@@ -13,7 +13,7 @@ class User extends Authenticatable
     use HasApiTokens, HasFactory, Notifiable;
 
     protected $fillable = [
-        'username',
+        'email',
         'password',
         'first_name',
         'last_name',
--- server/database/migrations/2014_10_12_000000_create_users_table.php
+++ server/database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('username');
+            $table->string('email')->unique();
             $table->string('password');
             $table->string('first_name');
             $table->string('last_name');
