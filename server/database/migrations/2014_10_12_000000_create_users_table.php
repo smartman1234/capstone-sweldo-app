92c478da10c2cad26edadc2f48516f34643d6a58
smartman1234 <john.smith.dev0724@gmail.com>
2023-01-31 20:13:24 +0800

    updated user table

--- server/database/migrations/2014_10_12_000000_create_users_table.php
+++ server/database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,11 +15,15 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('username');
             $table->string('password');
-            $table->rememberToken();
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->date('birthday');
+            $table->enum('gender', ['male', 'female']);
+            $table->string('address');
+            $table->string('phone');
+            $table->boolean('is_admin')->default(false);
             $table->timestamps();
         });
     }
