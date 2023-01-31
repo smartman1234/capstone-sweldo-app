decccbef90cfd673b08a5f53cc487451e9a5a7d7
smartman1234 <john.smith.dev0724@gmail.com>
2023-01-31 20:31:41 +0800

    updated login page

--- client/src/pages/login.jsx
+++ client/src/pages/login.jsx
@@ -1,5 +1,28 @@
 const Login = () => {
-  return <div>Login Page</div>
+  return (
+    <div className=''>
+      <div>
+        <div className='mb-4'>
+          <h1 className='text-3xl font-bold'>Sweldo App</h1>
+        </div>
+        {/* Input */}
+        <div className='mb-8 space-y-4'>
+          <div>
+            <label htmlFor='username' className='block'>Username</label>
+            <input type='text' placeholder='Username' className='border' />
+          </div>
+          <div>
+            <label htmlFor='password' className='block'>Password</label>
+            <input type='password' placeholder='Password' className='border' />
+          </div>
+        </div>
+        {/* Login */}
+        <div>
+          <button>Login</button>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Login
