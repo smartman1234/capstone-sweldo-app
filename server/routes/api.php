c0441bbe2d80e73e0a6cfb705fa1119fe19c81f8
smartman1234 <john.smith.dev0724@gmail.com>
2023-01-31 21:03:55 +0800

    added admin dashboard

--- server/routes/api.php
+++ server/routes/api.php
@@ -1,6 +1,31 @@
 <?php
 
-use App\Http\Controllers\Auth\LoginController;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\Auth\LoginController;
+
+use App\Http\Controllers\Admin\AdminDashboardController;
+
 Route::post('/login', [LoginController::class, 'login']);
+
+/*
+    User routes
+*/
+
+/*
+    Admin routes
+*/
+
+Route::group([
+    'prefix' => '/admin',
+    'middleware' => [
+        'auth:api',
+        'admin'
+    ]
+], function () {
+
+    // Dashboard
+    Route::get('/dashboard', [AdminDashboardController::class, 'index']);
+
+
+});
