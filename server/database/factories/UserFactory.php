eb1d0bcc87bd56c4e11c1c6c5904e76cc61c79d5
smartman1234 <john.smith.dev0724@gmail.com>
2023-01-31 22:48:26 +0800

    updated database seeder

--- server/database/factories/UserFactory.php
+++ server/database/factories/UserFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use DateTime;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
 
@@ -18,11 +19,14 @@ class UserFactory extends Factory
     public function definition()
     {
         return [
-            'name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
-            'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-            'remember_token' => Str::random(10),
+            'first_name' => 'Test',
+            'last_name' => 'Account',
+            'birthday' => new DateTime(),
+            'gender' => 'male',
+            'address' => 'test address',
+            'phone' => 'phone',
         ];
     }
 
--- server/database/seeders/DatabaseSeeder.php
+++ server/database/seeders/DatabaseSeeder.php
@@ -3,7 +3,10 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use DateTime;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,11 +17,27 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        \App\Models\User::create([
+            'email' => 'admin@gmail.com',
+            'first_name' => 'Admin',
+            'last_name' => 'Account',
+            'password' => Hash::make('password'),
+            'birthday' => new DateTime(),
+            'gender' => 'male',
+            'address' => 'test address',
+            'phone' => 'phone',
+            'is_admin' => 1
+        ]);
+        $testacc = \App\Models\User::create([
+            'email' => 'test@gmail.com',
+            'first_name' => 'Test',
+            'last_name' => 'Account',
+            'password' => Hash::make('password'),
+            'birthday' => new DateTime(),
+            'gender' => 'male',
+            'address' => 'test address',
+            'phone' => 'phone',
+        ]);
+        \App\Models\User::factory(100)->create();
     }
 }
