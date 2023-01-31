6c0dfbc4af9fc767ad249da41d6bf9cf64fa625f
smartman1234 <john.smith.dev0724@gmail.com>
2023-01-31 20:57:56 +0800

    added attendance overview

--- client/src/components/admin/dashboard/Statistics.jsx
+++ client/src/components/admin/dashboard/Statistics.jsx
@@ -2,26 +2,30 @@ import React from 'react'
 
 const Statistics = () => {
   return (
-    <div className='bg-slate-200 m-5 p-5'>
-      <h1 className='font-bold mb-5'>Statistics</h1>
-      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-5'>
-        <div className='bg-slate-500 h-24 p-3'>
-          <h2 className='mb-4'>Total Employees</h2>
-          <h2 className='flex justify-center text-2xl'>20</h2>
+    <div>
+        
+        <div className='bg-slate-200 m-5 p-5'>
+          <h1 className='font-bold mb-5'>Statistics</h1>
+          <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-5'>
+            <div className='bg-slate-500 h-24 p-3'>
+              <h2 className='mb-4'>Total Employees</h2>
+              <h2 className='flex justify-center text-2xl'>20</h2>
+            </div>
+            <div className='bg-slate-500 h-24 p-3'>
+              <h2 className='mb-4'>Present</h2>
+              <h2 className='flex justify-center text-2xl'>16/20</h2>
+            </div>
+            <div className='bg-slate-500 h-24 p-3'>
+              <h2 className='mb-4'>Late</h2>
+              <h2 className='flex justify-center text-2xl'>3/20</h2>
+            </div>
+            <div className='bg-slate-500 h-24 p-3'>
+              <h2 className='mb-4'>On Leave</h2>
+              <h2 className='flex justify-center text-2xl'>1/20</h2>
+            </div>
+          </div>
         </div>
-        <div className='bg-slate-500 h-24 p-3'>
-          <h2 className='mb-4'>Present</h2>
-          <h2 className='flex justify-center text-2xl'>16/20</h2>
-        </div>
-        <div className='bg-slate-500 h-24 p-3'>
-          <h2 className='mb-4'>Late</h2>
-          <h2 className='flex justify-center text-2xl'>3/20</h2>
-        </div>
-        <div className='bg-slate-500 h-24 p-3'>
-          <h2 className='mb-4'>On Leave</h2>
-          <h2 className='flex justify-center text-2xl'>1/20</h2>
-        </div>
-      </div>
+          <div className="h-96 bg-slate-200 m-5 p-5">Attendance Overview</div>
     </div>
   )
 }
