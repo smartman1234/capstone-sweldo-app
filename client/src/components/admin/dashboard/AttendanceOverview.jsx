3973a89a053cf9af42315fbb5c8dd41a2c715609
smartman1234 <john.smith.dev0724@gmail.com>
2023-02-01 22:43:57 +0800

    added button

--- client/src/components/admin/dashboard/AttendanceOverview.jsx
+++ client/src/components/admin/dashboard/AttendanceOverview.jsx
@@ -1,6 +1,24 @@
+import { useState } from 'react'
+import CustomButton from '../../ui/buttons/CustomButton'
 
 const AttendanceOverview = () => {
-  return <div className='h-96 bg-slate-200 m-5 p-5'>Attendance Overview</div>
+  const [toggle, setToggle] = useState(false)
+
+  const toggleButton = async () => {
+    setToggle(!toggle)
+  }
+  return (
+    <div className='h-96 bg-slate-200 m-5 p-5'>
+      <div className='flex justify-between'>
+        <h1 className='font-bold'>Attendance Overview</h1>
+        {toggle ? (
+          <CustomButton name='Clock Out' onClick={toggleButton} />
+        ) : (
+          <CustomButton name='Clock In' onClick={toggleButton} />
+        )}
+      </div>
+    </div>
+  )
 }
 
 export default AttendanceOverview
