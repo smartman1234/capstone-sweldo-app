576d7c66d1ecbb03640c1a3535c961bc3b818726
smartman1234 <john.smith.dev0724@gmail.com>
2023-02-01 22:39:14 +0800

    updated task model and migration

--- server/app/Models/Task.php
+++ server/app/Models/Task.php
@@ -11,6 +11,7 @@ class Task extends Model
 
     protected $fillable = [
         'name',
+        'date',
     ];
 
     public function user()
--- server/database/migrations/2023_02_01_142843_create_tasks_table.php
+++ server/database/migrations/2023_02_01_142843_create_tasks_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->id();
             $table->foreignId('user_id')->constrained();
             $table->string('name');
+            $table->timestamp('date');
             $table->timestamps();
         });
     }
