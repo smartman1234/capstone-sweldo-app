1f706b6aa215d21d515ac7c84f0e3b37696953a6
smartman1234 <john.smith.dev0724@gmail.com>
2023-02-01 10:03:53 +0800

    update employee store

--- server/app/Http/Controllers/Admin/EmployeeController.php
+++ server/app/Http/Controllers/Admin/EmployeeController.php
@@ -16,7 +16,14 @@ class EmployeeController extends Controller
     public function store(Request $request)
     {
         // TODO: Save employee
-        
+        $email = $request->email;
+        $password = $request->password;
+        $first_name = $request->first_name;
+        $last_name = $request->last_name;
+        $birthday = $request->birthday;
+        $gender = $request->gender;
+        $address = $request->address;
+        $phone = $request->phone;
     }
 
     public function show(Request $request)
