ef3a8895bf04edd6b02541f1a11aa789f605cb89
smartman1234 <john.smith.dev0724@gmail.com>
2023-02-01 11:20:51 +0800

    updated leave controller

--- server/app/Http/Controllers/Admin/LeaveController.php
+++ server/app/Http/Controllers/Admin/LeaveController.php
@@ -11,6 +11,17 @@ class LeaveController extends Controller
     public function index(Request $request)
     {
         $leaves = Leave::paginate(10);
+        $employeesName = [];
+        foreach ($leaves->items() as $item) {
+            $employeesName[] = [
+                'id' => $item->id,
+                'name' => $item->user->first_name,
+                'date' => $item->date,
+                'status' => $item->status,
+            ];
+        }
+        $leaves = $leaves->toArray();
+        $leaves['data'] = $employeesName;
         return response()->json([
             'leaves' => $leaves,
         ]);
