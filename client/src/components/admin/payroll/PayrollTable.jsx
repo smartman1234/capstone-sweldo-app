8eeb208e0fc11d9d15058298fca5710fa151e11d
smartman1234 <john.smith.dev0724@gmail.com>
2023-02-01 21:44:23 +0800

    updated payroll

--- client/src/components/admin/payroll/PayrollTable.jsx
+++ client/src/components/admin/payroll/PayrollTable.jsx
@@ -1,32 +1,28 @@
-import React from 'react'
-import PageTitle from '../../ui/titles/PageTitle'
-
 const PayrollTable = () => {
   return (
     <div>
-        <table className='w-full text-left'>
-      <thead className='bg-gray-100 uppercase'>
-        <tr>
-          <th className='p-2.5'>#</th>
-          <th className='p-2.5'>Employee Name</th>
-          <th className='p-2.5'>Total Hours</th>
-          <th className='p-2.5'>Deduction</th>
-          <th className='p-2.5'>Earning</th>
-          <th className='p-2.5'>Action</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
+      <table className='w-full text-left'>
+        <thead className='bg-gray-100 uppercase'>
+          <tr>
+            <th className='p-2.5'>#</th>
+            <th className='p-2.5'>Name</th>
+            <th className='p-2.5'>Total Hours</th>
+            <th className='p-2.5'>Deduction</th>
+            <th className='p-2.5'>Earning</th>
+            <th className='p-2.5'>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
             <td className='p-2.5'>1</td>
             <td className='p-2.5'>John Doe</td>
             <td className='p-2.5'>462</td>
             <td className='p-2.5'>352</td>
             <td className='p-2.5'>700</td>
-        </tr>
+          </tr>
         </tbody>
-        </table>
-
-        </div>
+      </table>
+    </div>
   )
 }
 
--- client/src/pages/admin/payroll.jsx
+++ client/src/pages/admin/payroll.jsx
@@ -1,16 +1,42 @@
-import GeneratePayroll from '../../components/admin/payroll/GeneratePayroll'
+import { useState } from 'react'
 import PayrollTable from '../../components/admin/payroll/PayrollTable'
+import CustomButton from '../../components/ui/buttons/CustomButton'
+import CustomInput from '../../components/ui/inputs/CustomInput'
 import PageTitle from '../../components/ui/titles/PageTitle'
 
 const Payroll = () => {
+  const [formData, setFormData] = useState({
+    month: '',
+  })
+
   return (
     <div>
       <PageTitle title='Payroll' />
-
-      <div className="grid gap-5">
-
-      <GeneratePayroll />
-      <PayrollTable />
+      <div className='space-y-4'>
+        <div className='flex justify-end space-x-4'>
+          <div>
+            <CustomInput
+              id='month'
+              type='month'
+              value={formData.month}
+              onChange={(e) => {
+                setFormData({ ...formData, month: e.target.value })
+                // TODO: Filter payrolls
+              }}
+            />
+          </div>
+          <CustomButton
+            name='Generate Payroll'
+            onClick={() => {}}
+            loading={false}
+          />
+          <CustomButton
+            name='Generate Payslip'
+            onClick={() => {}}
+            loading={false}
+          />
+        </div>
+        <PayrollTable />
       </div>
     </div>
   )
