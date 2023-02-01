8965d66dda53139aab3c4c04ab3582fb125ee5d6
smartman1234 <john.smith.dev0724@gmail.com>
2023-02-01 21:51:17 +0800

    updated user relationship

--- server/app/Models/User.php
+++ server/app/Models/User.php
@@ -28,6 +28,11 @@ class User extends Authenticatable
         'password',
     ];
 
+    public function attendances()
+    {
+        return $this->hasMany(Attendance::class);
+    }
+
     public function leaves()
     {
         return $this->hasMany(Leave::class);
