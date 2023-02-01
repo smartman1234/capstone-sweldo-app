ef9cd1adb4d739b481c3ad1e38d2206e7ebfb1e9
smartman1234 <john.smith.dev0724@gmail.com>
2023-02-01 14:29:37 +0800

    added leave table

---
+++ client/src/components/admin/leave/LeaveTable.jsx
@@ -0,0 +1,80 @@
+import { useState } from 'react'
+import ActionButton from '../../ui/buttons/ActionButton'
+import * as RestApi from '../../../utils/rest_api_util'
+
+const LeaveTable = ({ leaves, getLeaves }) => {
+  const [loading, setLoading] = useState(false)
+
+  const approveLeave = async (id) => {
+    setLoading(true)
+    try {
+      const result = await RestApi.approveLeave(id)
+      const response = await result.json()
+      if (result.status === 200) {
+        getLeaves(leaves.current_page)
+      }
+    } catch (error) {}
+    setLoading(false)
+  }
+
+  const declineLeave = async (id) => {
+    setLoading(true)
+    try {
+      const result = await RestApi.declineLeave(id)
+      const response = await result.json()
+      if (result.status === 200) {
+        getLeaves(leaves.current_page)
+      }
+    } catch (error) {}
+    setLoading(false)
+  }
+
+  return (
+    <table className='w-full text-left'>
+      <thead className='bg-gray-100 uppercase'>
+        <tr>
+          <th className='p-2.5'>#</th>
+          <th className='p-2.5'>Email</th>
+          <th className='p-2.5'>Name</th>
+          <th className='p-2.5'>Date</th>
+          <th className='p-2.5'>Status</th>
+          <th className='p-2.5'>Action</th>
+        </tr>
+      </thead>
+      <tbody>
+        {leaves !== undefined &&
+          (leaves.data.length !== 0 ? (
+            leaves.data.map((leave, index) => (
+              <tr key={index} className='border-b'>
+                <th className='p-2.5'>{leaves.from + index}</th>
+                <td className='p-2.5'>{leave.email}</td>
+                <td className='p-2.5'>{leave.name}</td>
+                <td className='p-2.5'>{leave.date}</td>
+                <td className='p-2.5'>{leave.status}</td>
+                <td className='p-2.5 space-x-4'>
+                  <ActionButton
+                    name='Approve'
+                    onClick={() => approveLeave(leave.id)}
+                    loading={loading}
+                  />
+                  <ActionButton
+                    name='Decline'
+                    onClick={() => declineLeave(leave.id)}
+                    loading={loading}
+                  />
+                </td>
+              </tr>
+            ))
+          ) : (
+            <tr>
+              <td colSpan='5' className='text-center p-2.5'>
+                No leave available
+              </td>
+            </tr>
+          ))}
+      </tbody>
+    </table>
+  )
+}
+
+export default LeaveTable
--- client/src/pages/admin/leave.jsx
+++ client/src/pages/admin/leave.jsx
@@ -1,15 +1,14 @@
 import { useEffect, useState } from 'react'
-import ActionButton from '../../components/ui/buttons/ActionButton'
 import CustomInput from '../../components/ui/inputs/CustomInput'
 import PageTitle from '../../components/ui/titles/PageTitle'
 import Pagination from '../../components/Pagination'
+import LeaveTable from '../../components/admin/leave/LeaveTable'
 import * as RestApi from '../../utils/rest_api_util'
 
 const Leave = () => {
   const [formData, setFormData] = useState({
     name: '',
   })
-  const [loading, setLoading] = useState(false)
 
   const [leaves, setLeaves] = useState()
 
@@ -41,30 +40,6 @@ const Leave = () => {
     } catch (error) {}
   }
 
-  const approveLeave = async (id) => {
-    setLoading(true)
-    try {
-      const result = await RestApi.approveLeave(id)
-      const response = await result.json()
-      if (result.status === 200) {
-        getLeaves(leaves.current_page)
-      }
-    } catch (error) {}
-    setLoading(false)
-  }
-
-  const declineLeave = async (id) => {
-    setLoading(true)
-    try {
-      const result = await RestApi.declineLeave(id)
-      const response = await result.json()
-      if (result.status === 200) {
-        getLeaves(leaves.current_page)
-      }
-    } catch (error) {}
-    setLoading(false)
-  }
-
   return (
     <div>
       <PageTitle title='Leave' />
@@ -79,50 +54,10 @@ const Leave = () => {
             searchLeaves(e.target.value)
           }}
         />
-        <table className='w-full text-left'>
-          <thead className='bg-gray-100 uppercase'>
-            <tr>
-              <th className='p-2.5'>#</th>
-              <th className='p-2.5'>Email</th>
-              <th className='p-2.5'>Name</th>
-              <th className='p-2.5'>Date</th>
-              <th className='p-2.5'>Status</th>
-              <th className='p-2.5'>Action</th>
-            </tr>
-          </thead>
-          <tbody>
-            {leaves !== undefined &&
-              (leaves.data.length !== 0 ? (
-                leaves.data.map((leave, index) => (
-                  <tr key={index} className='border-b'>
-                    <th className='p-2.5'>{leaves.from + index}</th>
-                    <td className='p-2.5'>{leave.email}</td>
-                    <td className='p-2.5'>{leave.name}</td>
-                    <td className='p-2.5'>{leave.date}</td>
-                    <td className='p-2.5'>{leave.status}</td>
-                    <td className='p-2.5 space-x-4'>
-                      <ActionButton
-                        name='Approve'
-                        onClick={() => approveLeave(leave.id)}
-                        loading={loading}
-                      />
-                      <ActionButton
-                        name='Decline'
-                        onClick={() => declineLeave(leave.id)}
-                        loading={loading}
-                      />
-                    </td>
-                  </tr>
-                ))
-              ) : (
-                <tr>
-                  <td colSpan='5' className='text-center p-2.5'>
-                    No leave available
-                  </td>
-                </tr>
-              ))}
-          </tbody>
-        </table>
+        <LeaveTable
+          leaves={leaves}
+          getLeaves={getLeaves}
+        />
         <Pagination pagination={leaves} onClick={getLeaves} />
       </div>
     </div>
