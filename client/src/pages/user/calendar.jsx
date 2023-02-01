cea974d1e55371b3cd81ca4708b7d8e36f866858
smartman1234 <john.smith.dev0724@gmail.com>
2023-02-01 22:19:40 +0800

    added calendar

--- client/src/pages/user/calendar.jsx
+++ client/src/pages/user/calendar.jsx
@@ -1,5 +1,13 @@
+import PageTitle from '../../components/ui/titles/PageTitle'
+import CalendarForm from '../../components/user/calendar/CalendarForm'
+
 const Calendar = () => {
-  return <div>Calendar</div>
+  return (
+    <div>
+      <PageTitle title='Calendar' />
+      <CalendarForm />
+    </div>
+  )
 }
 
 export default Calendar
