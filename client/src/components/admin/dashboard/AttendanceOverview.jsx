2a88acc9a4731abc04c83f81be81344eff61ec9a
smartman1234 <john.smith.dev0724@gmail.com>
2023-02-01 22:55:19 +0800

    added components

--- client/src/components/admin/dashboard/AttendanceOverview.jsx
+++ client/src/components/admin/dashboard/AttendanceOverview.jsx
@@ -1,11 +1,38 @@
 import { useState } from 'react'
 import CustomButton from '../../ui/buttons/CustomButton'
+import * as RestApi from '../../../utils/rest_api_util'
 
 const AttendanceOverview = () => {
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState()
+  const [success, setSuccess] = useState()
   const [toggle, setToggle] = useState(false)
 
   const toggleButton = async () => {
     setToggle(!toggle)
+  }
+
+  const clockIn = async () => {
+    try {
+      const result = await RestApi.clockIn()
+      const response = await result.json()
+      if (result.status === 200) {
+      }
+
+      if (result.status === 400) {
+      }
+    } catch (error) {}
+  }
+  const clockOut = async () => {
+    try {
+      const result = await RestApi.clockOut()
+      const response = await result.json()
+      if (result.status === 200) {
+      }
+
+      if (result.status === 400) {
+      }
+    } catch (error) {}
   }
   return (
     <div className='h-96 bg-slate-200 m-5 p-5'>
