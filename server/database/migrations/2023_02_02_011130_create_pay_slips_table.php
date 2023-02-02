1ac5f7f901963d09eafc7b98bf8415aead00cc66
smartman1234 <john.smith.dev0724@gmail.com>
2023-02-02 09:12:42 +0800

    added payslip migration

---
+++ server/database/migrations/2023_02_02_011130_create_pay_slips_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pay_slips', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained();
+            $table->float('total_hours');
+            $table->float('deductions');
+            $table->float('earnings');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pay_slips');
+    }
+};
