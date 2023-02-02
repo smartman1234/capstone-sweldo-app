f96aab57ec7fa17493c094c80dc24248208759be
smartman1234 <john.smith.dev0724@gmail.com>
2023-02-02 09:43:55 +0800

    added clockin logic

--- client/src/components/admin/dashboard/AttendanceOverview.jsx
+++ client/src/components/admin/dashboard/AttendanceOverview.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import CustomButton from '../../ui/buttons/CustomButton'
 import * as RestApi from '../../../utils/rest_api_util'
+import SuccessAlert from '../../ui/alerts/SuccessAlert'
 
 const AttendanceOverview = ({ isClockIn }) => {
   const [loading, setLoading] = useState(false)
@@ -8,40 +9,51 @@ const AttendanceOverview = ({ isClockIn }) => {
   const [success, setSuccess] = useState()
   const [toggle, setToggle] = useState(false)
 
-  const toggleButton = async () => {
-    setToggle(!toggle)
-  }
-
   const clockIn = async () => {
+    setLoading(true)
+    setError(undefined)
+    setSuccess(undefined)
     try {
       const result = await RestApi.clockIn()
       const response = await result.json()
       if (result.status === 200) {
+        setSuccess(response)
       }
 
       if (result.status === 400) {
+        setError(response)
       }
     } catch (error) {}
+    setLoading(false)
   }
   const clockOut = async () => {
     try {
       const result = await RestApi.clockOut()
       const response = await result.json()
       if (result.status === 200) {
+        setSuccess(response)
       }
 
       if (result.status === 400) {
+        setError(response)
       }
     } catch (error) {}
+    setLoading(false)
   }
   return (
     <div className='h-96 bg-slate-200 m-5 p-5'>
       <div className='flex justify-between'>
         <h1 className='font-bold'>Attendance Overview</h1>
-        {toggle ? (
-          <CustomButton name='Clock Out' onClick={toggleButton} />
+        {isClockIn ? (
+          <div>
+            <CustomButton name='Clock Out' onClick={clockOut} loading={loading} />
+            <SuccessAlert message={success.message} />
+          </div>
         ) : (
-          <CustomButton name='Clock In' onClick={toggleButton} />
+          <div>
+            <CustomButton name='Clock In' onClick={clockIn} loading={loading} />
+            <SuccessAlert message={success.message} />
+          </div>
         )}
       </div>
     </div>
