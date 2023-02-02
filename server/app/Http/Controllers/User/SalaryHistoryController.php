a32a99fcbed51f8f46644f76c2edd2455d0ca35d
smartman1234 <john.smith.dev0724@gmail.com>
2023-02-02 09:07:08 +0800

    updated salary history controller

--- server/app/Http/Controllers/User/SalaryHistoryController.php
+++ server/app/Http/Controllers/User/SalaryHistoryController.php
@@ -3,11 +3,40 @@
 namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 class SalaryHistoryController extends Controller
 {
     public function index(Request $request)
     {
+        // Get user
+        $user = $request->user();
+
+        // Get all attendance
+        $attendances = $user->attendances()->get();
+
+        // Get total hours and rate
+        $salaryHistory = [];
+        foreach ($attendances as $attendance) {
+            $month = Carbon::parse($attendance->created_at)->format('M Y');
+            if (!isset($salaryHistory[$month])) {
+                $salaryHistory[$month] = [
+                    'date' => $month,
+                    'totalHours' => 0,
+                    'rate' => $user->job->salary,
+                ];
+            }
+            $salaryHistory[$month]['totalHours'] += Carbon::parse($attendance->clock_in)->diffInHours(Carbon::parse($attendance->clock_out));
+        }
+
+        $final = [];
+        foreach ($salaryHistory as $salary) {
+            $final[] = $salary;
+        }
+
+        return response()->json([
+            'salaryHistory' => $final
+        ]);
     }
 }
