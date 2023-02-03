06db8d2c7a5410c98326317b339e77c16956fc89
smartman1234 <john.smith.dev0724@gmail.com>
2023-02-03 13:02:34 +0800

    added reset data command

---
+++ server/app/Console/Commands/ResetData.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Artisan;
+
+class ResetData extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'command:resetdata';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        Artisan::call('migrate:fresh');
+        Artisan::call('db:seed');
+        Artisan::call('passport:install');
+
+        return Command::SUCCESS;
+    }
+}
