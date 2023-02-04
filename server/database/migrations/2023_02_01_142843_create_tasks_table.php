a75777052677c199f5711cf0389d328d8296255d
smartman1234 <john.smith.dev0724@gmail.com>
2023-02-04 22:41:27 +0800

    added onDelete on constrained

--- server/database/migrations/2023_02_01_142843_create_tasks_table.php
+++ server/database/migrations/2023_02_01_142843_create_tasks_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('tasks', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->constrained();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->string('name');
             $table->timestamp('date');
             $table->timestamps();
