b58bb3bd3a458ff8f86b53776547822c8f4788b6
smartman1234 <john.smith.dev0724@gmail.com>
2023-02-04 22:41:18 +0800

    added onDelete on constrained

--- server/database/migrations/2023_02_01_134859_create_attendances_table.php
+++ server/database/migrations/2023_02_01_134859_create_attendances_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('attendances', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->constrained();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->timestamp('clock_in')->nullable();
             $table->timestamp('clock_out')->nullable();
             $table->timestamps();
