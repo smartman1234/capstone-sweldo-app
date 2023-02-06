4eb16348294ad3fdf1740d431e6211f4becedddb
smartman1234 <john.smith.dev0724@gmail.com>
2023-02-06 09:22:33 +0800

    added icon in upload image

--- client/src/pages/admin/admin_profile.jsx
+++ client/src/pages/admin/admin_profile.jsx
@@ -110,7 +110,26 @@ const AdminProfile = () => {
               className='w-20 h-20 rounded-full'
             />
           )}
+          <label htmlFor='avatar' className='flex gap-5'>
+            <svg
+              xmlns='http://www.w3.org/2000/svg'
+              fill='none'
+              viewBox='0 0 24 24'
+              strokeWidth={1.5}
+              stroke='currentColor'
+              className='w-6 h-6'
+            >
+              <path
+                strokeLinecap='round'
+                strokeLinejoin='round'
+                d='M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5m-13.5-9L12 3m0 0l4.5 4.5M12 3v13.5'
+              />
+            </svg>
+            Upload Avatar
+          </label>
           <input
+            hidden
+            id='avatar'
             type='file'
             accept='image/*'
             onChange={(e) => {
