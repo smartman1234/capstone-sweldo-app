fe1d369778ff55e9bf403fa7229496b957df59b7
smartman1234 <john.smith.dev0724@gmail.com>
2023-02-07 11:02:53 +0800

    added index

---
+++ server/app/Http/Controllers/Admin/AdminAttendanceController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\Attendance;
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+
+class AdminAttendanceController extends Controller
+{
+
+    public function index()
+    {
+
+        $attendances = Attendance::orderBy('clock_in', 'desc')->take(10)->get();
+
+        $employeesName = [];
+        foreach ($attendances as $attendance) {
+            $employeesName[] = [
+                'id' => $attendance->id,
+                'email' => $attendance->user->email,
+                'name' => $attendance->user->first_name . ' ' . $attendance->user->last_name,
+                'clock_in' => $attendance->clock_in,
+                'status' => $attendance->clock_in >= Carbon::now()->setTime(9, 15, 0) ? 'late' : 'present',
+            ];
+        }
+
+        return response()->json([
+            'attendance' => $employeesName,
+        ]);
+    }
+}
