6a7e23fd3ab37987fa92afbf149bbd7752846949
smartman1234 <john.smith.dev0724@gmail.com>
2023-02-07 14:57:42 +0800

    updated admin attendance

--- client/src/pages/admin/admin_attendance.jsx
+++ client/src/pages/admin/admin_attendance.jsx
@@ -1,4 +1,6 @@
 import React, { useEffect, useState } from 'react'
+import AdminAttendanceTable from '../../components/admin/attendance/AdminAttendanceTable'
+import Pagination from '../../components/Pagination'
 import CustomInput from '../../components/ui/inputs/CustomInput'
 import PageTitle from '../../components/ui/titles/PageTitle'
 import * as RestApi from '../../utils/rest_api_util'
@@ -22,7 +24,7 @@ const AdminAttendance = () => {
       const result = await RestApi.getAttendances(page)
       const response = await result.json()
       if (result.status === 200) {
-        setAttendances(response.employees)
+        setAttendances(response.attendances)
       }
     } catch (error) {}
   }
@@ -32,16 +34,16 @@ const AdminAttendance = () => {
       const result = await RestApi.searchAttendances(name, page)
       const response = await result.json()
       if (result.status === 200) {
-        setAttendances(response.attendance)
+        setAttendances(response.attendances)
       }
     } catch (error) {}
   }
+
   return (
     <div>
-
-    <PageTitle title='Attendance'/>
-    <div className='space-y-4'>
-        <div className='flex space-x-4'>
+      <PageTitle title='Attendance' />
+      <div className='space-y-4'>
+        <div className='flex'>
           <CustomInput
             id='name'
             type='text'
@@ -52,58 +54,10 @@ const AdminAttendance = () => {
               searchAttendances(e.target.value)
             }}
           />
-          </div>
-    <table className='w-full text-left'>
-      <thead className='bg-gray-100 uppercase'>
-        <tr>
-          <th className='p-2.5'>#</th>
-          <th className='p-2.5'>Name</th>
-          <th className='p-2.5'>Clock In</th>
-          <th className='p-2.5'>Clock Out</th>
-          <th className='p-2.5'>Date</th>
-          <th className='p-2.5'>Status</th>
-        </tr>
-      </thead>
-      <tbody>
-        {attendances !== undefined &&
-          (attendances.length !== 0 ? (
-            attendances.map((attendance, index) => (
-              <tr key={index} className='border-b'>
-                <th className='p-2.5'>{index + 1}</th>
-                <td className='p-2.5'>{attendance.name}</td>
-                <td className='p-2.5'>{attendance.clock_in}</td>
-                <td className='p-2.5'>{attendance.clock_out}</td>
-                <td className='p-2.5'>
-                  {new Date(attendance.clock_in).toLocaleDateString('default', {
-                    month: 'long',
-                    day: 'numeric',
-                    year: 'numeric',
-                  })}
-                </td>
-                <td className='p-2.5'>
-                  {attendance.status === 'present' && (
-                    <span className='bg-green-500 text-white text-sm capitalize rounded px-2 py-1'>
-                      {attendance.status}
-                    </span>
-                  )}
-                  {attendance.status === 'late' && (
-                    <span className='bg-red-500 text-white text-sm capitalize rounded px-2 py-1'>
-                      {attendance.status}
-                    </span>
-                  )}
-                </td>
-              </tr>
-            ))
-          ) : (
-            <tr>
-              <td colSpan='5' className='text-center p-2.5'>
-                No leave available
-              </td>
-            </tr>
-          ))}
-      </tbody>
-    </table>
-    </div>
+        </div>
+        <AdminAttendanceTable attendances={attendances} />
+        <Pagination pagination={attendances} onClick={getAttendances} />
+      </div>
     </div>
   )
 }
--- server/app/Http/Controllers/Admin/AdminAttendanceController.php
+++ server/app/Http/Controllers/Admin/AdminAttendanceController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use App\Models\Attendance;
-use App\Models\User;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
 
@@ -14,32 +13,38 @@ class AdminAttendanceController extends Controller
     public function index(Request $request)
     {
         if ($request->name == null) {
-            $employees = User::paginate(10);
+            $attendances = Attendance::paginate(10);
         } else {
-            $employees = User::with('first_name')->paginate(10);
+            $attendances = Attendance::with('user')
+                ->whereHas('user', function ($query) use ($request) {
+                    $query->where('first_name', 'LIKE', "%" . $request->name . "%")
+                        ->orWhere('last_name', 'LIKE', "%" . $request->name . "%");
+                })
+                ->paginate(10);
         }
 
-        $attendances = Attendance::orderBy('clock_in', 'desc')->take(10)->get();
-
         $employeesName = [];
-        foreach ($attendances as $attendance) {
+        foreach ($attendances->items() as $item) {
             $employeesName[] = [
-                'id' => $attendance->id,
-                'name' => $attendance->user->first_name . ' ' . $attendance->user->last_name,
-                'clock_in' => $attendance->clock_in,
-                'status' => $attendance->clock_in >= Carbon::now()->setTime(9, 15, 0) ? 'late' : 'present',
+                'id' => $item->id,
+                'name' => $item->user->first_name . ' ' . $item->user->last_name,
+                'date' => $item->date,
+                'clock_in' => $item->clock_in,
+                'clock_out' => $item->clock_out,
+                'total_hours' => Carbon::parse($item->clock_in)->diffInHours(Carbon::parse($item->clock_out)),
+                'status' => $item->clock_in >= Carbon::now()->setTime(9, 15, 0) ? 'late' : 'present',
             ];
         }
 
+        $attendances = $attendances->toArray();
+        $attendances['data'] = $employeesName;
         return response()->json([
-            'employees' => $employees,
-            'attendance' => $employeesName,
+            'attendances' => $attendances,
         ]);
     }
 
     public function getRecentAttendance()
     {
-
         $attendances = Attendance::orderBy('clock_in', 'desc')->take(10)->get();
 
         $employeesName = [];
