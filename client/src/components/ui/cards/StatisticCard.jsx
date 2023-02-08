54a3d121fae9987be1fdcce0e5dc6681a10ad257
smartman1234 <john.smith.dev0724@gmail.com>
2023-02-08 09:16:28 +0800

    styling statistic card

--- client/src/components/ui/cards/StatisticCard.jsx
+++ client/src/components/ui/cards/StatisticCard.jsx
@@ -1,8 +1,8 @@
 const StatisticCard = ({ name, value }) => {
   return (
-    <div className='bg-white shadow p-5'>
-      <div className='text-xl text-center'>{name}</div>
-      <h2 className='text-3xl text-center font-bold'>{value}</h2>
+    <div className='bg-sky-700 shadow p-5'>
+      <div className='text-xl text-white text-center'>{name}</div>
+      <h2 className='text-3xl text-white text-center font-bold'>{value}</h2>
     </div>
   )
 }
