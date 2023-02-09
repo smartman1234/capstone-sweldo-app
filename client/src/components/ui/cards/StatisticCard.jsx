3e564484484fcee2b0ea28f3baddfb4780e37925
smartman1234 <john.smith.dev0724@gmail.com>
2023-02-09 10:58:41 +0800

    added icons

--- client/src/components/ui/cards/StatisticCard.jsx
+++ client/src/components/ui/cards/StatisticCard.jsx
@@ -1,11 +1,13 @@
 const StatisticCard = ({ name, value, svg }) => {
   return (
-    <div className='bg-white shadow p-5 drop-shadow-xl rounded-lg hover:scale-105 duration-200'>
-      <div className='font-bold text-gray-700 flex justify-between uppercase'>
-        {name}
-        {svg}
+    <div className='bg-white flex space-x-4 shadow p-5 drop-shadow-xl rounded-lg hover:scale-105 duration-200'>
+      <div className='bg-gray-100 p-2 rounded-lg'>{svg}</div>
+      <div>
+        <div className='font-semibold text-sm text-gray-400 flex justify-between uppercase'>
+          {name}
+        </div>
+        <h2 className='text-3xl text-[#20262E] font-bold'>{value}</h2>
       </div>
-      <h2 className='text-3xl text-black font-bold'>{value}</h2>
     </div>
   )
 }
--- client/src/pages/user/dashboard.jsx
+++ client/src/pages/user/dashboard.jsx
@@ -44,27 +44,84 @@ const UserDashboard = () => {
         <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4'>
           <StatisticCard
             name='Total Hours (Monthly)'
+            value={stats.monthly}
             svg={
               <svg
                 xmlns='http://www.w3.org/2000/svg'
                 fill='none'
                 viewBox='0 0 24 24'
-                stroke-width='1.5'
+                strokeWidth='1.5'
                 stroke='currentColor'
-                class='w-6 h-6'
+                className='h-full'
               >
                 <path
-                  stroke-linecap='round'
-                  stroke-linejoin='round'
+                  strokeLinecap='round'
+                  strokeLinejoin='round'
                   d='M12 6v6h4.5m4.5 0a9 9 0 11-18 0 9 9 0 0118 0z'
                 />
               </svg>
             }
-            value={stats.monthly}
           />
-          <StatisticCard name='Rate per hour' value={stats.rate} />
-          <StatisticCard name='Total Leave' value={stats.leave} />
-          <StatisticCard name='Expected Salary' value={stats.expectedSalary} />
+          <StatisticCard
+            name='Rate per hour'
+            value={stats.rate}
+            svg={
+              <svg
+                xmlns='http://www.w3.org/2000/svg'
+                fill='none'
+                viewBox='0 0 24 24'
+                strokeWidth='1.5'
+                stroke='currentColor'
+                className='h-full'
+              >
+                <path
+                  strokeLinecap='round'
+                  strokeLinejoin='round'
+                  d='M2.25 18.75a60.07 60.07 0 0115.797 2.101c.727.198 1.453-.342 1.453-1.096V18.75M3.75 4.5v.75A.75.75 0 013 6h-.75m0 0v-.375c0-.621.504-1.125 1.125-1.125H20.25M2.25 6v9m18-10.5v.75c0 .414.336.75.75.75h.75m-1.5-1.5h.375c.621 0 1.125.504 1.125 1.125v9.75c0 .621-.504 1.125-1.125 1.125h-.375m1.5-1.5H21a.75.75 0 00-.75.75v.75m0 0H3.75m0 0h-.375a1.125 1.125 0 01-1.125-1.125V15m1.5 1.5v-.75A.75.75 0 003 15h-.75M15 10.5a3 3 0 11-6 0 3 3 0 016 0zm3 0h.008v.008H18V10.5zm-12 0h.008v.008H6V10.5z'
+                />
+              </svg>
+            }
+          />
+          <StatisticCard
+            name='Total Leave'
+            value={stats.leave}
+            svg={
+              <svg
+                xmlns='http://www.w3.org/2000/svg'
+                fill='none'
+                viewBox='0 0 24 24'
+                strokeWidth='1.5'
+                stroke='currentColor'
+                className='h-full'
+              >
+                <path
+                  strokeLinecap='round'
+                  strokeLinejoin='round'
+                  d='M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15M12 9l-3 3m0 0l3 3m-3-3h12.75'
+                />
+              </svg>
+            }
+          />
+          <StatisticCard
+            name='Expected Salary'
+            value={stats.expectedSalary}
+            svg={
+              <svg
+                xmlns='http://www.w3.org/2000/svg'
+                fill='none'
+                viewBox='0 0 24 24'
+                strokeWidth='1.5'
+                stroke='currentColor'
+                className='h-full'
+              >
+                <path
+                  strokeLinecap='round'
+                  strokeLinejoin='round'
+                  d='M12 6v12m-3-2.818l.879.659c1.171.879 3.07.879 4.242 0 1.172-.879 1.172-2.303 0-3.182C13.536 12.219 12.768 12 12 12c-.725 0-1.45-.22-2.003-.659-1.106-.879-1.106-2.303 0-3.182s2.9-.879 4.006 0l.415.33M21 12a9 9 0 11-18 0 9 9 0 0118 0z'
+                />
+              </svg>
+            }
+          />
         </div>
         <AttendanceOverview clock={clock} setClock={setClock} />
       </div>
