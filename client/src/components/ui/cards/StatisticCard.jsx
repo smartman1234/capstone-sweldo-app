1bb15cb687cbd29cd0871d20e75347fd316e834f
smartman1234 <john.smith.dev0724@gmail.com>
2023-02-09 10:38:26 +0800

    added svg in statistic card

--- client/src/components/ui/cards/StatisticCard.jsx
+++ client/src/components/ui/cards/StatisticCard.jsx
@@ -1,8 +1,11 @@
-const StatisticCard = ({ name, value }) => {
+const StatisticCard = ({ name, value, svg }) => {
   return (
     <div className='bg-white shadow p-5 drop-shadow-xl rounded-lg hover:scale-105 duration-200'>
-      <div className='font-bold text-gray-700 text-center uppercase'>{name}</div>
-      <h2 className='text-3xl text-black text-center font-bold'>{value}</h2>
+      <div className='font-bold text-gray-700 flex justify-between uppercase'>
+        {name}
+        {svg}
+      </div>
+      <h2 className='text-3xl text-black font-bold'>{value}</h2>
     </div>
   )
 }
--- client/src/pages/user/dashboard.jsx
+++ client/src/pages/user/dashboard.jsx
@@ -42,7 +42,26 @@ const UserDashboard = () => {
       <PageTitle title='Dashboard' />
       <div className='space-y-4'>
         <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4'>
-          <StatisticCard name='Total Hours (Monthly)' value={stats.monthly} />
+          <StatisticCard
+            name='Total Hours (Monthly)'
+            svg={
+              <svg
+                xmlns='http://www.w3.org/2000/svg'
+                fill='none'
+                viewBox='0 0 24 24'
+                stroke-width='1.5'
+                stroke='currentColor'
+                class='w-6 h-6'
+              >
+                <path
+                  stroke-linecap='round'
+                  stroke-linejoin='round'
+                  d='M12 6v6h4.5m4.5 0a9 9 0 11-18 0 9 9 0 0118 0z'
+                />
+              </svg>
+            }
+            value={stats.monthly}
+          />
           <StatisticCard name='Rate per hour' value={stats.rate} />
           <StatisticCard name='Total Leave' value={stats.leave} />
           <StatisticCard name='Expected Salary' value={stats.expectedSalary} />
