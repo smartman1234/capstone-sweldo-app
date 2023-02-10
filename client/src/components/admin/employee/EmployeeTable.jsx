30b44fcfc765569e7301d3ab570a8811da91e715
smartman1234 <john.smith.dev0724@gmail.com>
2023-02-10 23:42:41 +0800

    updated design

--- client/src/components/admin/employee/EmployeeTable.jsx
+++ client/src/components/admin/employee/EmployeeTable.jsx
@@ -65,7 +65,7 @@ const EmployeeTable = ({ employees, setSelectedEmployeeId, getEmployees }) => {
             ))
           ) : (
             <tr>
-              <td colSpan='5' className='text-center p-2.5'>
+              <td colSpan='6' className='text-center p-2.5'>
                 No data available
               </td>
             </tr>
