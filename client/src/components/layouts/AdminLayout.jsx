2823507c653182a0c7d6f29a11b2e52eb189fa75
smartman1234 <john.smith.dev0724@gmail.com>
2023-02-10 22:27:55 +0800

    updated admin layout

--- client/src/components/layouts/AdminLayout.jsx
+++ client/src/components/layouts/AdminLayout.jsx
@@ -37,7 +37,7 @@ const AdminLayout = () => {
         }
       >
         <Navbar activeSidebar={activeSidebar} toggleSidebar={toggleSidebar} />
-        <div className='p-5'>
+        <div className='py-8 px-5'>
           <Outlet />
         </div>
       </div>
